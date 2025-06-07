fdad208cdc8479b8f55a58ee123b83189b203a36
Cyrine <sireen.ahmad.45@gmail.com>
2025-06-07 23:43:50 +0200

    feat: added Express server setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const { port } = require('./config/config');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/', authRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
