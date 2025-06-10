7dab55f1d884a0f7ac039ba6418ca487b3da9f56
Cyrine <sireen.ahmad.45@gmail.com>
2025-06-10 13:36:22 +0200

    chore(api): switch weather provider from RapidAPI to Open-Meteo
    - Update base URLs for weather and geocoding APIs
    - Remove RapidAPI dependencies
    - Prepare for Open-Meteo integration

--- WeatherApp/src/app/Environment/EnvironmentalVariables.ts
+++ WeatherApp/src/app/Environment/EnvironmentalVariables.ts
@@ -1,12 +1,5 @@
 export const EnvironmentalVariables = {
   production: false,
-  weatherApiLocationBaseURL:
-    'https://weather338.p.rapidapi.com/locations/search?',
-  weatherApiForecastBaseURL:
-    'https://weather338.p.rapidapi.com/weather/forecast?',
-  xRapidApiKeyName: 'x-rapidapi-key',
-  xRapidApiKeyValue: '336cfc7112mshcd5eeee455e0cefp15a9b8jsn8eaf5bfe7df5',
-  xRapidApiHostName: 'x-rapidapi-host',
-  xRapidApiHostValue: 'weather338.p.rapidapi.com',
+  weatherApiBaseURL: 'https://api.open-meteo.com/v1/',
+  geocodingApiBaseURL: 'https://geocoding-api.open-meteo.com/v1/',
 };
- 
