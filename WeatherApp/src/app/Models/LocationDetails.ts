5188dde6c47f9471e3cee1d3c53c3ffdbbfe9551
Cyrine <sireen.ahmad.45@gmail.com>
2025-06-10 13:37:25 +0200

    refactor(models): update LocationDetails for Open-Meteo compatibility

--- WeatherApp/src/app/Models/LocationDetails.ts
+++ WeatherApp/src/app/Models/LocationDetails.ts
@@ -1,41 +1,18 @@
 export interface LocationDetails {
-  location: Location;
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  elevation: number;
+  feature_code: string;
+  country_code: string;
+  admin1?: string;
+  admin2?: string;
+  admin3?: string;
+  admin4?: string;
+  timezone: string;
+  population?: number;
+  postcodes?: string[];
+  country_id: number;
+  country: string;
 }
-
-export interface Location {
-  address: string[];
-  adminDistrict: string | undefined[];
-  adminDistrictCode: any[];
-  city: string[];
-  country: string[];
-  countryCode: string[];
-  displayName: string[];
-  displayContext: string | undefined[];
-  ianaTimeZone: string[];
-  latitude: number[];
-  locale: Locale[];
-  longitude: number[];
-  neighborhood: any[];
-  placeId: string[];
-  postalCode: string | undefined[];
-  postalKey: string | undefined[];
-  disputedArea: boolean[];
-  disputedCountries: string[] | undefined[];
-  disputedCountryCodes: string[] | undefined[];
-  disputedCustomers: any[][] | undefined[];
-  disputedShowCountry: boolean[][];
-  iataCode: string | undefined[];
-  icaoCode: string | undefined[];
-  locId: string[];
-  locationCategory: any[];
-  pwsId: string | undefined[];
-  type: string[];
-}
-
-export interface Locale {
-  locale1?: string;
-  locale2: string;
-  locale3?: string;
-  locale4: any;
-}
- 
